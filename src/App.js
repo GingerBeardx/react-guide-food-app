50b35e1e6d1e34f87d71b1eae989d8731d289004
Eric Greenhalgh <egreenhalgh@gmail.com>
2023-06-02 16:02:48 -0600

    Added modal open and close functionality

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import { Header, Meals, Cart } from "./components";
 
 function App() {
+  const [showCart, setShowCart] = useState(false);
+
+  const handleShowCart = () => {
+    setShowCart((prev) => !prev);
+  };
+
   return (
     <React.Fragment>
-      <Cart />
-      <Header />
+      {showCart && <Cart onClose={handleShowCart} />}
+      <Header onOpen={handleShowCart} />
       <main>
         <Meals />
       </main>
