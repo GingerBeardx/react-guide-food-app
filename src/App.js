510143e43bde09dbbe31aac290b8b61d7a9eea0f
Eric Greenhalgh <egreenhalgh@gmail.com>
2023-06-02 09:56:08 -0600

    Added meals and meals summary

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
-import { Header } from "./components";
+import { Header, Meals } from "./components";
 
 function App() {
   return (
     <React.Fragment>
       <Header />
+      <main>
+        <Meals />
+      </main>
     </React.Fragment>
   );
 }
--- src/components/index.js
+++ src/components/index.js
@@ -1,1 +1,2 @@
 export { default as Header } from "./Layout/Header";
+export { default as Meals } from "./Meals/Meals";
