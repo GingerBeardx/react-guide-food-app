f991c21a8450802c8112eedce5269069b64becee
Eric Greenhalgh <egreenhalgh@gmail.com>
2023-06-02 11:29:10 -0600

    Added the Input component

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 export { default as Header } from "./Layout/Header";
 export { default as Meals } from "./Meals/Meals";
 export { default as Card } from "./UI/Card";
+export { default as Input } from "./UI/Input";
