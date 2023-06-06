107768b861e1924f037ae746b60318c2381e2ad3
Eric Greenhalgh <egreenhalgh@gmail.com>
2023-06-06 14:13:56 -0600

    Added bump effect to cart button

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,17 +1,37 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import styles from "./HeaderCartButton.module.css";
 import CartContext from "../../store/cart-context";
 import CartIcon from "../Cart/CartIcon";
 
 const HeaderCartButton = (props) => {
   const cartCtx = useContext(CartContext);
+  const [buttonHighlight, setButtonHighight] = useState(false);
 
-  const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
+  const { items } = cartCtx;
+
+  const numberOfCartItems = items.reduce((curNumber, item) => {
     return curNumber + item.amount;
   }, 0);
 
+  const btnClasses = `${styles.button} ${buttonHighlight ? styles.bump : ""}`;
+
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+    setButtonHighight(true);
+
+    const timer = setTimeout(() => {
+      setButtonHighight(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
+
   return (
-    <button className={styles.button} onClick={props.onOpen}>
+    <button className={btnClasses} onClick={props.onOpen}>
       <span className={styles.icon}>
         <CartIcon />
       </span>
